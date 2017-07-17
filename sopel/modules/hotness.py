2eb5f5f4e7a034652a0da8edb561e2d3bd2a642e
Alan <Alan.Huang@utdallas.edu>
2017-07-16 20:32:34 -0500

    made 'kinsey' command set sexuality

--- sopel/modules/hotness.py
+++ sopel/modules/hotness.py
@@ -12,7 +12,34 @@ def hot_command(bot, trigger):
         if randint(0,2) == 0:
             bot.say('{0}/10 with rice'.format(randint(0,10)))
 
-@commands('gay', 'kinsey')
+@commands('gay')
 def gay_command(bot,trigger):
-    rand = randint(0, 6)
-    bot.say('Kinsey Scale Rating: {0}/6'.format(rand))
+    if not trigger.group(2):
+        nick = trigger.nick
+    else:
+        nick = trigger.group(2).strip()
+    val = bot.db.get_nick_value(nick, 'gay')
+    if not val:
+        rand = randint(0, 6)
+        bot.say('Kinsey Scale Rating: {0}/6'.format(rand))
+    else:
+        bot.say('Kinsey Scale Rating: {0}/6'.format(val))
+
+@commands('kinsey')
+def kinsey_set(bot, trigger):
+    '''Set a value for the .gay command.'''
+    if not trigger.group(2):
+        bot.db.set_nick_value(trigger.nick, 'gay', None)
+        bot.say('Value cleared.')
+        return
+    if trigger.group(2).lower() == 'x':
+        val = 'X'
+    else:
+        try:
+            val = int(trigger.group(2))
+            assert val < 7 and val > -1
+        except:
+            bot.say('Enter a valid value [0-6]')
+            return
+    bot.db.set_nick_value(trigger.nick, 'gay', val)
+    bot.say('Value set.')
