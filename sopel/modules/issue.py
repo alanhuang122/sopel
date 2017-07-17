6a9b3f81c0c04ff0cdc6494b5f17a509cfd271d9
Alan <Alan.Huang@utdallas.edu>
2017-07-16 20:32:51 -0500

    Fix overwriting issues

--- sopel/modules/issue.py
+++ sopel/modules/issue.py
@@ -8,6 +8,6 @@ def issue_command(bot, trigger):
     """Report problems with the bot"""
     if trigger.group(2):
         bot.say("Issue \"{0}\" filed to /dev/null".format(trigger.group(2)))
-        with open('/home/alan/.sopel/issues', 'w') as file:
+        with open('/home/alan/.sopel/issues', 'a') as file:
             file.write(trigger.raw)
             file.close()
