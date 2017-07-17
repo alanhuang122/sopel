d1960421de8d102b15c991929407b8aeda681057
Alan <Alan.Huang@utdallas.edu>
2017-07-16 20:33:09 -0500

    add ability to reverse lookup (provisional)

--- sopel/modules/nicks.py
+++ sopel/modules/nicks.py
@@ -9,11 +9,6 @@ def equal(a, b):
     except AttributeError:
         return a == b
 
-
-def chunkstring(string, length):
-    return (string[0 + i:length + i] for i in range(0, len(string), length))
-
-
 @commands('who', 'whois', 'username')
 @example('.whois Alan')
 def lookup_command(bot, trigger):
@@ -24,6 +19,23 @@ def lookup_command(bot, trigger):
     for row in reader:
         if equal(trigger.group(2).strip(), row[0].strip()):
             string = row[2]
+            break
+
+    if not string:
+        bot.say('Nick was not found in pronoun doc. https://goo.gl/q6CJQg')
+        print trigger.group(2)
+        return
+    bot.say(string)
+
+@commands('rwho')
+def rwho_command(bot,trigger):
+    """Returns the IGN of a user whose nick is in the pronoun sheet: https://goo.gl/q6CJQg"""
+    url = 'http://docs.google.com/feeds/download/spreadsheets/Export?key=1l_fLXnNA-jeKFuh-o6wLBxNf_YX3Tyci-brqvkYSfaQ&exportFormat=csv'
+    reader = csv.reader(urllib2.urlopen(url), dialect='excel')
+    string = None
+    for row in reader:
+        if trigger.group(2).strip().lower() in row[2].strip().lower():
+            string = str(row)
             break
 
     if not string:
