ae1e70fe94867dc8a1e10c0b8eb06bcccd111f19
Alan <Alan.Huang@utdallas.edu>
2017-10-06 17:36:38 -0500

    Not sure when I broke this, but it's fixed now.

--- sopel/modules/dictionary.py
+++ sopel/modules/dictionary.py
@@ -7,36 +7,29 @@ Licensed under the Eiffel Forum License 2.
 http://sopel.chat
 """
 from __future__ import unicode_literals, absolute_import, print_function, division
-import requests, re, aspell
+import requests
 from sopel.module import commands, example
 
 url = 'https://od-api.oxforddictionaries.com/api/v1'
 
-def setup(bot):
-    global app_id, app_key
-    app_id = bot.config.dict.id
-    app_key = bot.config.dict.key
-
-def search(word):
-    global app_id, app_key
-    data = requests.get(url + '/search/en', params = {'q': word.lower(), 'limit': 1}, headers = {'app_id': app_id, 'app_key': app_key}).json()
+def search(word, id, key):
+    data = requests.get(url + '/search/en', params = {'q': word.lower(), 'limit': 1}, headers = {'app_id': id, 'app_key': key}).json()
     if data['results'] == []:
         return None
     return data['results'][0]['id']
     
-def lookup(word):
+def lookup(word, id, key):
 #    s = aspell.Speller()
 #    if not s.check(word):
 #        if len(s.suggest(word)) is 0:
 #            return "I couldn't find any definitions for that word."
 #        word = s.suggest(word)[0]
 
-    word_id = search(word)
+    word_id = search(word, id, key)
     if word_id is None:
         return 'I couldn\'t find any definitions for that word.'
 
-    global app_id, app_key
-    data = requests.get(url + '/entries/en/' + word_id + '/definitions', headers = {'app_id' : app_id, 'app_key' : app_key})
+    data = requests.get(url + '/entries/en/' + word_id + '/definitions', headers = {'app_id' : id, 'app_key' : key})
     if data.status_code == 404:
         return 'I couldn\'t find that word.'
     data = data.json()
@@ -44,7 +37,6 @@ def lookup(word):
     word = data['results'][0]['word']
     lexicalEntries = data['results'][0]['lexicalEntries']
     count = len(lexicalEntries)
-    limit = 400 / count
 
     for entry in lexicalEntries:
         temp = []
@@ -76,6 +68,6 @@ def dictionary(bot, trigger):
         bot.reply('You must tell me what to look up!')
         return
 
-    msg = lookup(word)
+    msg = lookup(word, bot.config.dict.id, bot.config.dict.key)
     bot.say(msg)
     return
