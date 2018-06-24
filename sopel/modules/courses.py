4a3344d1a16f53d99dddc2b1c071d94f67fd3fac
Alan Huang <Alan.Huang@utdallas.edu>
2018-06-23 21:49:32 -0500

    courses: create command for parsing course info

--- sopel/modules/courses.py
+++ sopel/modules/courses.py
@@ -1,8 +1,9 @@
 # 2016.12.24 03:29:54 CST
 #Embedded file name: modules/courses.py
-from sopel.module import commands, example
-import requests
 from lxml.html import fromstring
+import re
+import requests
+from sopel.module import commands, example
 
 @commands('coursebook', 'cb', 'class', 'course')
 @example('.cb cs2336')
@@ -12,9 +13,47 @@ def course_command(bot, trigger):
     url = 'https://catalog.utdallas.edu/now/undergraduate/courses/{}'.format(course)
     data = requests.get(url)
     tree = fromstring(data.content)
-    tag = tree.find(".//div[@id='bukku-page']/p")
-    text = tag.text_content()
-    if not text:
+    title = tree.find(".//div[@id='bukku-page']/h1").text_content()
+    if not title:
         bot.say("Course not found.")
         return
-    bot.say(text)
+    desc = tree.find(".//div[@id='bukku-page']/p").text_content()
+    print(desc)
+    hourstring = re.search(r'(\(.+?\))', desc).group(1)
+    try:
+        print(desc.rsplit('Prerequisite', 1)[1].rsplit(None, 2))
+        prereqs, hours, freq = desc.rsplit('Prerequisite', 1)[1].rsplit(None, 2)
+        prereqs = prereqs.replace('.', '')
+        print(prereqs)
+    except IndexError:
+        prereqs = None
+        _, hours, freq = desc.rsplit(None, 2)
+    lecture, lab = hours[1:-1].rsplit('-', 1)
+    if freq == 'S':
+        frequency = 'Offered at least once per long semester'
+    elif freq == 'Y':
+        frequency = 'Offered at least once per year'
+    elif freq == 'T':
+        frequency = 'Offered at least once every two years'
+    elif freq == 'R':
+        frequency = 'Offered based on demand'
+    else:
+        frequency = 'Unknown frequency {}'.format(freq)
+    if prereqs:
+        string = '{} {} ({} hours lecture, {} hours lab) Prerequisite{}; Frequency: {}'.format(title, hourstring, lecture, lab, prereqs, frequency)
+    else:
+        string = '{} {} ({} hours lecture, {} hours lab); Frequency: {}'.format(title, hourstring, lecture, lab, frequency)
+    bot.say(string)
+
+@commands('desc')
+def desc_command(bot, trigger):
+    course = trigger.group(2).replace(' ', '').lower()
+    url = 'https://catalog.utdallas.edu/now/undergraduate/courses/{}'.format(course)
+    data = requests.get(url)
+    tree = fromstring(data.content)
+    desc = tree.find(".//div[@id='bukku-page']/p").text_content()
+    if not desc:
+        bot.say('Course not found.')
+        return
+    desc = desc.rsplit('Prerequisites', 1)[0]
+    bot.say(desc)
