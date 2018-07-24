e704058d88abf0100789271f396ab1be9bde0b4a
Alan Huang <Alan.Huang@utdallas.edu>
2018-07-24 10:01:34 -0500

    isup: remove functionality to check invalid sites example: .ping me -> https://apple.com

--- sopel/modules/isup.py
+++ sopel/modules/isup.py
@@ -21,15 +21,16 @@ def isup(bot, trigger):
             site = 'http://' + site
 
     if not '.' in site:
-        site += ".com"
+        bot.say("I don't think that's a valid site.")
+        return
 
     try:
         site = site.replace('http', 'https', 1)
-        response = requests.get(site)
+        response = requests.get(site, allow_redirects=False)
     except requests.exceptions.SSLError:
         try:
             site = site.replace('https', 'http', 1)
-            response = requests.get(site)
+            response = requests.get(site, allow_redirects=False)
         except Exception:
             bot.say(site + ' looks down from here.')
             return
