1216ba80862d44dd406bc6c3c1f83dbdaf3fda1a
Alan Huang <Alan.Huang@utdallas.edu>
2018-07-24 10:00:11 -0500

    status_codes: add handler for empty trigger

--- sopel/modules/status_codes.py
+++ sopel/modules/status_codes.py
@@ -1,8 +1,19 @@
 from sopel.module import commands
+from sopel.trigger import PreTrigger
 from http.client import responses
 
 @commands('http')
 def http_command(bot, trigger):
+    if not trigger.group(2):
+        bot.say('https://twitter.com/stevelosh/status/372740571749572610')
+        parts = trigger.raw.split(None)
+        parts = parts[:4]
+        parts.append(':https://twitter.com/stevelosh/status/372740571749572610')
+        string = ' '.join(parts)
+
+        pt = PreTrigger(bot.nick, string)
+        bot.dispatch(pt)
+        return
     try:
         code = int(trigger.group(2))
         bot.say('HTTP {}: {}'.format(code, responses[code]))
