01104c7a98b4228b2d71a88abbc1f685f776a752
Alan Huang <Alan.Huang@utdallas.edu>
2018-07-24 10:03:03 -0500

    horoscope: clean up imports

--- sopel/modules/horoscope.py
+++ sopel/modules/horoscope.py
@@ -2,9 +2,9 @@
 #coding: latin-1
 
 from sopel.module import commands
-from sopel.modules import emoticons
+from sopel.modules.emoticons import taunt_command as taunt
 
 @commands('horoscope')
 def horoscope_command(bot, trigger):
-    emoticons.taunt_command(bot,trigger)
+    taunt(bot, trigger)
     return
